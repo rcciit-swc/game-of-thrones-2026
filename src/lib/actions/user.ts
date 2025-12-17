e7b87cd0aa9b3fec24acebf4481da883e4f88e29
Anirban Majumder <81350357+Anirban-Majumder@users.noreply.github.com>
2025-12-18 03:17:50 +0530

    fix: improve user data handling and add logging in profile functions

--- src/lib/actions/user.ts
+++ src/lib/actions/user.ts
@@ -3,12 +3,24 @@ import { getUserData, updateUserData } from '@/utils/functions';
 export const populateUserDetails = async (set: any) => {
   set({ userLoading: true });
   const data = await getUserData();
-  set({ userData: data, userLoading: false });
+  if (data) {
+    set({ userData: data.data, swcData: data.swcData, userLoading: false });
+  } else {
+    set({ userLoading: false });
+  }
 };
 
 export const update_and_populate = async (set: any, data: any) => {
   set({ userLoading: true });
   await updateUserData(data);
   const updatedData = await getUserData();
-  set({ userData: updatedData, userLoading: false });
+  if (updatedData) {
+    set({
+      userData: updatedData.data,
+      swcData: updatedData.swcData,
+      userLoading: false,
+    });
+  } else {
+    set({ userLoading: false });
+  }
 };
