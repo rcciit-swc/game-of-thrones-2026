9f4bb1c278b4577bec6edc6151eb17f69c347c70
Anirban Majumder <81350357+Anirban-Majumder@users.noreply.github.com>
2025-12-17 17:02:19 +0530

    minor fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Hero from '@/components/Hero';
-import Navbar from '@/components/navbar';
 import Playground from '@/components/Playground';
-import React from 'react';
 import About from '@/components/about/about';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
@@ -10,10 +8,8 @@ const page = () => {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
       <Navbar />
-
       <Hero />
       <About />
-
       <Playground />
       <Footer />
     </div>
