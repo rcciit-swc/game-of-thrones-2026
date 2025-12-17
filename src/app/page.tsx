403be737f32f845a4c5589590bed93a6b1805d77
omsohom01 <omsohom01@gmail.com>
2025-12-18 00:15:07 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@ import Hero from '@/components/Hero';
 import Navbar from '@/components/navbar';
 import Playground from '@/components/Playground';
 import React from 'react';
-import About from '@/components/about/about';
-import Footer from '@/components/Footer';
-import Navbar from '@/components/Navbar';
+import About from '@/components/about/About';
+import Footer from '@/components/footer';
 
 const page = () => {
   return (
