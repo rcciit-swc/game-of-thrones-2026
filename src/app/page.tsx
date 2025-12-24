7ddc71fc0609d75c5550c92fce3dbd772aad84bd
Soumyaraj Bag <soumyarajbag@gmail.com>
2025-12-25 00:08:54 +0530

    add:loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,41 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import Hero from '@/components/Hero';
 import About from '@/components/about/about';
 import Playground from '@/components/Playground';
+import LoaderOverlay from '@/components/events/LoaderOverlay';
+import { useLoaderStore } from '@/lib/stores/loader';
 
-const page = () => {
+const Page = () => {
+  const [showLoader, setShowLoader] = useState(false);
+  const { hasVisited, setHasVisited } = useLoaderStore();
+
+  useEffect(() => {
+    if (!hasVisited) {
+      setShowLoader(true);
+    }
+  }, [hasVisited]);
+
+  const handleLoaderComplete = () => {
+    setShowLoader(false);
+    setHasVisited(true);
+  };
+
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
-      <Hero />
-      <About />
-      <Playground />
-    </div>
+    <>
+      <LoaderOverlay
+        isVisible={showLoader}
+        onAnimationComplete={handleLoaderComplete}
+      />
+
+      <div className="flex flex-col min-h-screen overflow-hidden">
+        <Hero />
+        <About />
+        <Playground />
+      </div>
+    </>
   );
 };
 
-export default page;
+export default Page;
